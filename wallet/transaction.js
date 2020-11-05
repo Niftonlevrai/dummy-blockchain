98b4f810d7d78894af8332d1e8cbb5689fafeff4
mariustache <mariusalexandrutache@gmail.com>
2020-11-05 21:23:34 +0200

    Added method for transaction validation.

--- wallet/transaction.js
+++ wallet/transaction.js
@@ -1,5 +1,6 @@
 
 const uuid = require('uuid');
+const { verifySignature } = require('../util');
 
 class Transaction {
     constructor({ senderWallet, recipient, amount }) {
@@ -29,6 +30,29 @@ class Transaction {
             address: senderWallet.publicKey,
             signature: senderWallet.sign(outputMap)
         };
+    }
+
+    static validTransaction(transaction) {
+        const { input: { address, amount, signature }, outputMap } = transaction;
+        
+        // Reconstruct total amount from recipients' values.
+        const outputTotal = Object.values(outputMap).reduce(
+            (total, outputAmount) => total + outputAmount
+        );
+
+        // Check if the sum of all outputMap recipient values matches
+        // the input amount.
+        if (amount !== outputTotal) {
+            console.error(`Invalid transaction from ${address}`);
+            return false;
+        }
+
+        if (!verifySignature({ publicKey: address, data: outputMap, signature })) {
+            console.error(`Invalid signature from ${address}`);
+            return false;
+        }
+
+        return true;
     }
 }
 
