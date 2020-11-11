4deb1f046d3515e6c7a96e4c3622177ed18f636c
mariustache <mariusalexandrutache@gmail.com>
2020-11-11 21:08:06 +0200

    Added sync for transaction pool.

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const app = express();
 const blockchain = new Blockchain();
 const transactionPool = new TransactionPool();
 const wallet = new Wallet();
-const pubsub = new PubSub({ blockchain });
+const pubsub = new PubSub({ blockchain, transactionPool });
 
 const DEFAULT_PORT = 3000;
 const ROOT_NODE_ADDRESS = `http://localhost:${DEFAULT_PORT}`;
@@ -51,10 +51,14 @@ app.post('/api/transact', (req, res) => {
     }
 
     transactionPool.setTransaction(transaction);
-
-    console.log('transactionPool', transactionPool);
+    // Notify peers that a new transaction has been created.
+    pubsub.broadcastTransaction(transaction);
 
     res.json({ type: 'success', transaction });
+});
+
+app.get('/api/transaction-pool-map', (req, res) => {
+    res.json(transactionPool.transactionMap);
 });
 
 const syncChains = () => {
